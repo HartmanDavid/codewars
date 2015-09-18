f574c86a38b41085614681c4e2eb19f27cfc1474
David Hartman <davidhartman00@gmail.com>
2015-09-17 22:50:40 -0700

    codewars problem 4 - I realize Im good at this but need to learn more about making my code more refined

--- basic-stuff.js
+++ basic-stuff.js
@@ -50,3 +50,34 @@ function insertDash(num) {
   return arrFinal.join('');
 }
   insertDash(122334455)
+
+// 4th Origin problem:
+// The Collatz Conjecture states that for any natural
+// number n, if n is even, divide it by 2. If n is odd,
+// multiply it by 3 and add 1. If you repeat the process
+// continously for n, n will eventually reach 1.
+//
+// For example, if n = 20, the resulting sequence will be:
+//
+// [20, 10, 5, 16, 8, 4, 2, 1]
+//
+// Write a program that will output the length of the Collatz
+// Conjecture for any given n. In the example above, the output
+// would be 8.
+//
+// Answer - Yes
+function collatz(n){
+  var arr = [];
+  for (var i = n; i > 1; i = n){
+      if (n % 2 === 0){
+        n = n / 2;
+        console.log('eve',n);
+        arr.push(n);
+      }else{
+        n = (n * 3) + 1;
+        console.log('odd',n);
+        arr.push(n);
+      }
+    }
+    return (arr.length + 1);
+}
